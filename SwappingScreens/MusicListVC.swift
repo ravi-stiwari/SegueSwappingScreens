2a9d4eff5982f9dd472da358cc97a3fc50cf6db7
Ravi Tiwari <rtiwari@digite.com>
2017-03-05 21:20:00 +0530

    Added segues for going back

--- SwappingScreens/MusicListVC.swift
+++ SwappingScreens/MusicListVC.swift
@@ -16,12 +16,10 @@ class MusicListVC: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func backButtonPressed(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
     }
     
-
     /*
     // MARK: - Navigation
 
