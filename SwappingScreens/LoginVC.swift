a8f692f7a0560fffdb4ff542ce1cbe63553df2d0
Ravi Tiwari <rtiwari@digite.com>
2017-03-05 21:09:49 +0530

    Committing Initial Changes with Login View

---
+++ SwappingScreens/LoginVC.swift
@@ -0,0 +1,18 @@
+//
+//  LoginVC.swift
+//  SwappingScreens
+//
+//  Created by Ravi Tiwari on 3/5/17.
+//  Copyright © 2017 SelfStudy. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
+
